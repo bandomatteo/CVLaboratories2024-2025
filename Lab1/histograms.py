ca22e98ba73726022b8b43fb827e94ade6d1343f
Niccolò Bisagno <niccolo.bisagno@unitn.it>
2025-03-20 16:32:03 +0100

    Update histograms.py
    added RGB histogram

--- Lab1/histograms.py
+++ Lab1/histograms.py
@@ -6,10 +6,37 @@ from matplotlib import pyplot as plt
 #GRAYSCALE
 img = cv2.imread('../material/home.jpg', cv2.IMREAD_GRAYSCALE)
 assert img is not None, "file could not be read, check with os.path.exists()"
+cv2.imshow('img',img)
+cv2.waitKey(0)
+
 hist = cv2.calcHist([img],[0],None,[256],[0,255])
 
-
 plt.hist(img.ravel(),256,[0,256]); plt.show()
+
+
+#RGB
+# Load image in color
+img = cv2.imread('../material/home.jpg')
+assert img is not None, "File could not be read, check with os.path.exists()"
+
+# Split channels
+b, g, r = cv2.split(img)
+
+# Compute histograms
+hist_b = cv2.calcHist([b], [0], None, [256], [0, 256])
+hist_g = cv2.calcHist([g], [0], None, [256], [0, 256])
+hist_r = cv2.calcHist([r], [0], None, [256], [0, 256])
+
+# Plot histograms
+plt.figure(figsize=(8, 6))
+plt.plot(hist_r, color='red', label='Red Channel')
+plt.plot(hist_g, color='green', label='Green Channel')
+plt.plot(hist_b, color='blue', label='Blue Channel')
+plt.title('RGB Histograms')
+plt.xlabel('Pixel Intensity')
+plt.ylabel('Frequency')
+plt.legend()
+plt.show()
 
 #CALCULATE HISTOGRAM FROM A REGION
 img = cv2.imread('../material/home.jpg', cv2.IMREAD_GRAYSCALE)
